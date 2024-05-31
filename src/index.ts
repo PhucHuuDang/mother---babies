7852de1e0028d9920d1f5f165ddaeec39f761c4c
ATHT138 <anhthtservice@gmail.com>
2024-05-31 18:53:34 +0700

    Done login and register api

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
+import router from "./router";
+
+const localhost = "localhost";
+const PORT = 8080;
 
 const app = express();
 
@@ -20,13 +24,15 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-  console.log("Server is running on port 8080");
+server.listen(PORT, () => {
+  console.log(`Server is running on http://${localhost}:${PORT}`);
 });
 
 // first: Trung Anh test db co chay on hay khong, neu khong Trung Anh co the change url nay thanh url local cua Trung Anh
-const MONGO_URL = "mongodb+srv://harry:harry@cluster0.p5hykkj.mongodb.net/";
+const MONGO_URL = "mongodb://localhost:27017";
 mongoose.Promise = Promise;
 
 mongoose.connect(MONGO_URL);
 mongoose.connection.on("error", (error: Error) => console.log({ error }));
+
+app.use("/", router());
