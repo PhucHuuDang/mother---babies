e4943e842420cebebbd1d50a10234a0fd5468843
ATHT138 <anhthtservice@gmail.com>
2024-06-03 10:25:05 +0700

    Remove node_module

--- src/controller/authentication.ts
+++ src/controller/authentication.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authentication, random } from "../helpers";
-import { createUser, getUserByEmail } from "../db/users";
+import { createUser, getUserByEmail } from "../db/users.model";
 
 export const login = async (req: express.Request, res: express.Response) => {
   try {
@@ -8,7 +8,7 @@ export const login = async (req: express.Request, res: express.Response) => {
 
     // Check if the email and password are provided
     if (!email || !password) {
-      return res.status(400);
+      return res.sendStatus(400);
     }
 
     const user = await getUserByEmail(email).select(
@@ -16,7 +16,7 @@ export const login = async (req: express.Request, res: express.Response) => {
     );
 
     if (!user) {
-      return res.status(400);
+      return res.sendStatus(400);
     }
 
     const { salt } = user.authentication;
@@ -43,35 +43,40 @@ export const login = async (req: express.Request, res: express.Response) => {
     return res.status(200).json(user).end();
   } catch (error) {
     console.log(error);
-    return res.status(400);
+    return res.sendStatus(400);
   }
 };
 
 export const register = async (req: express.Request, res: express.Response) => {
   try {
-    const { email, username, password } = req.body;
+    const { email, username, password, role, phone, address } = req.body;
 
     // Check if the email, username, and password are provided
     if (!email || !username || !password) {
-      return res.status(400);
+      console.log("Missing fields");
+
+      return res.sendStatus(400);
     }
 
     const existingUser = await getUserByEmail(email);
 
     if (existingUser) {
-      return res.status(400);
+      return res.sendStatus(400);
     }
 
     const salt = random();
     const user = await createUser({
       email,
       username,
+      role: role || "user",
+      phone,
+      address,
       authentication: { password: authentication(salt, password), salt },
     });
 
     return res.status(200).json(user).end();
   } catch (error) {
     console.log(error);
-    return res.status(400);
+    return res.sendStatus(400);
   }
 };
