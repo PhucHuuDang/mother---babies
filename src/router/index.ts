d8a06d9f202d3d16b5896c722844cdd5ff042539
ATHT138 <anhthtservice@gmail.com>
2024-07-02 18:05:38 +0700

    Fix: Open comment product import in router index

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,13 @@ import express from "express";
 
 import authentication from "./authentication";
 import users from "./users";
-// import product from "./product";
+import product from "./product";
 
 const router = express.Router();
 
 export default (): express.Router => {
   authentication(router);
   users(router);
-  // product(router);
+  product(router);
   return router;
 };
